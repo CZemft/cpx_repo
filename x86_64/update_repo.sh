f63badc581d559909f8ded7339564ac8d84ca651
CZemft <czbigboy4223@gmail.com>
2023-08-19 12:22:14 +1000

    monthly cleanup

---
+++ x86_64/update_repo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm cpx*
+
+echo "repo-add"
+repo-add -n -R cpx_repo.db.tar.gz *.pkg.tar.zst
+sleep 5
+
+echo "####################################"
+echo "Repo Updated!!"
+echo "####################################"
